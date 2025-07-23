9df8b0bc5f6b7f3f38079fe0a4ed74260ddd17c5
Lulu Zhou <lulu@eigenlabs.org>
2025-07-23 12:01:43 -0700

    update calculate_min_beta.py to allow different alpha values

--- calculate_min_beta.py
+++ calculate_min_beta.py
@@ -5,7 +5,6 @@ import math
 ## This is used to generate the table of the minimal beta_1 value for the data recovery in DAS
 
 # Given values
-alpha = 1/8
 gamma = 1/8
 c = 8192
 
@@ -21,22 +20,24 @@ def round_up_third_decimal(x):
     return math.ceil(x * 1000) / 1000
 
 # Given values for k
-k_values = [4, 5, 6, 7, 8, 12, 16, 20, 24]
+k_values = [8]
+alpha_values = [1/8, 1/6, 1/4, 1/3]
 
 # Calculate results for each k
 results = []
 
-for k in k_values:
-    gamma_k = gamma ** k
-    D_value = kullback_leibler_divergence(alpha, gamma_k)
-    H_value = entropy(gamma)
-    
-    # Solve for beta_1 from the inequality
-    # beta_1 >= (H_value + 0.011) / D_value
-    beta_min = (H_value + 0.011) / D_value
-    results.append([k, round_up_third_decimal(beta_min), math.ceil(beta_min*c)])
+for alpha in alpha_values:
+    for k in k_values:
+        gamma_k = gamma ** k
+        D_value = kullback_leibler_divergence(alpha, gamma_k)
+        H_value = entropy(gamma)
+        
+        # Solve for beta_1 from the inequality
+        # beta_1 >= (H_value + 0.011) / D_value
+        beta_min = (H_value + 0.011) / D_value
+        results.append([alpha, k, round_up_third_decimal(beta_min), math.ceil(beta_min*c)])
 
 # Create DataFrame
-df = pd.DataFrame(results, columns=["k", "Minimal Beta", "beta * c"])
+df = pd.DataFrame(results, columns=["Alpha", "k", "Minimal Beta", "beta * c"])
 
 print(df)
